57d255148a78efb571570a7d07064b50809c7e3a
Rachel Hunt <rsersh@gmail.com>
2019-10-28 15:05:16 -0700

    added published customer manager'

--- Djangodev/ecrivaine/blog/models.py
+++ Djangodev/ecrivaine/blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone 
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = ( ('draft', 'Draft'),('published', 'Published') )
     title = models.CharField(max_length=250)
@@ -12,10 +17,15 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    objects = models.Manager()
+    # The default manager
+    published = PublishedManager()  # Our customer manager
 
 class Meta:
     ordering = ('- publish',)
     def __str__ (self):
         return self.title
+        
+
 
 
